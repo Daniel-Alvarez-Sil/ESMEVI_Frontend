4017504cdbe3fea0ddc885d36499a5e26ae8deff
TECcoding <142265349+TECcoding@users.noreply.github.com>
2024-11-23 19:15:45 -0600

    Second try to update size box depending on the number of sensors

--- components/caliaire/Sensor.tsx
+++ components/caliaire/Sensor.tsx
@@ -24,45 +24,20 @@ const CaliAireSensor: React.FC = () => {
   }, []);
 
   return (
-    <div className="pt-2">
+    <div className="pt-2 grid gap-4 md:grid-cols-2 lg:grid-cols-2">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Sensores</CardTitle>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#dd1313"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-book-open-text"
-          >
-            <path d="M12 7v14" />
-            <path d="M16 12h2" />
-            <path d="M16 8h2" />
-            <path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" />
-            <path d="M6 12h2" />
-            <path d="M6 8h2" />
-          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#dd1313" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-book-open-text"><path d="M12 7v14"/><path d="M16 12h2"/><path d="M16 8h2"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/><path d="M6 12h2"/><path d="M6 8h2"/></svg>
         </CardHeader>
         <CardContent>
           <p className="text-xs text-muted-foreground pb-2">
             Componentes utilizados para medir.
           </p>
-          <div
-            className={`flex flex-wrap gap-4 justify-start ${
-              sensors.length > 6 ? "overflow-y-scroll max-h-64" : ""
-            }`}
-          >
+          <div className={`grid gap-4 ${sensors.length === 1 ? "grid-cols-1" : "md:grid-cols-2 lg:grid-cols-2"}`}>
             {sensors.length > 0 ? (
               sensors.map((sensor, index) => (
-                <Card
-                  key={index}
-                  className="flex-1 min-w-[120px] max-w-[150px]"
-                >
+                <Card key={index}>
                   <CardContent>
                     <div className="pt-3 text-2xl font-bold text-center">
                       {sensor}
